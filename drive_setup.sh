05b0bbc4de59bc8113d1c332e1e249b2dd84c3d0
Malachi Griffith <mgriffit@genome.wustl.edu>
2013-10-21 15:19:58 -0500

    Add partitioning and formatting steps for VDI disks sdb and sdc in VM provisioning file

--- drive_setup.sh
+++ drive_setup.sh
@@ -9,16 +9,26 @@ echo "Provisioning added block devices..."
 
 # This assumes that the /vagrant shared path
 # was correctly mounted during 'vagrant up'
+echo "Creating partition on /dev/sdb -> /dev/sdb1"
 sudo fdisk -l /dev/sdb | grep -q "^/dev/sdb1" || \
   fdisk /dev/sdb < /vagrant/fdisk_input.txt
-#sudo fdisk -l /dev/sdc | grep -q "^/dev/sdc1" || \
-#  fdisk /dev/sdc < /vagrant/fdisk_input.txt
+echo "Creating partition on /dev/sdc -> /dev/sdc1"
+sudo fdisk -l /dev/sdc | grep -q "^/dev/sdc1" || \
+  fdisk /dev/sdc < /vagrant/fdisk_input.txt
+echo "Creating partition on /dev/sdd -> /dev/sdd1"
+sudo fdisk -l /dev/sdd | grep -q "^/dev/sdd1" || \
+  fdisk /dev/sdd < /vagrant/fdisk_input.txt
 
 # Check for a filesystem, make one if needed
+echo "Formatting partition sdb1 as ext4"
 sudo dumpe2fs /dev/sdb1 >/dev/null 2>&1 || \
   mkfs.ext4 /dev/sdb1
-#sudo dumpe2fs /dev/sdc1 >/dev/null 2>&1 || \
-#  mkfs.ext4 /dev/sdc1
+echo "Formatting partition sdc1 as ext4"
+sudo dumpe2fs /dev/sdc1 >/dev/null 2>&1 || \
+  mkfs.ext4 /dev/sdc1
+echo "Formatting partition sdd1 as ext4"
+sudo dumpe2fs /dev/sdd1 >/dev/null 2>&1 || \
+  mkfs.ext4 /dev/sdd1
 
 #Mounting plan
 #/dev/sdb1 /opt/gms
