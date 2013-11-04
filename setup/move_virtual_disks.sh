4e2dd2959f5074298ec2a2dfe0a9064fce9ca354
Malachi Griffith <mgriffit@genome.wustl.edu>
2013-11-03 21:05:54 -0600

    Modify move virtual disk example to account for change in the path of this script

--- setup/move_virtual_disks.sh
+++ setup/move_virtual_disks.sh
@@ -26,24 +26,24 @@ CWD=$(dirname $0)
 
 echo
 echo "Moving temp virtual disk and creating a symlink to it"
-echo mv $CWD/tmp-disk.vdi $TMP_PATH/
-mv $CWD/tmp-disk.vdi $TMP_PATH/
-echo ln -s $TMP_PATH/tmp-disk.vdi $CWD/tmp-disk.vdi
-ln -s $TMP_PATH/tmp-disk.vdi $CWD/tmp-disk.vdi
+echo mv $CWD/../tmp-disk.vdi $TMP_PATH/
+mv $CWD/../tmp-disk.vdi $TMP_PATH/
+echo ln -s $TMP_PATH/tmp-disk.vdi $CWD/../tmp-disk.vdi
+ln -s $TMP_PATH/tmp-disk.vdi $CWD/../tmp-disk.vdi
 
 echo
 echo "Moving GMS virtual disk and creating a symlink to it"
-echo mv $CWD/opt-gms-disk.vdi $GMS_PATH/
-mv $CWD/opt-gms-disk.vdi $GMS_PATH/
-echo ln -s $GMS_PATH/opt-gms-disk.vdi $CWD/opt-gms-disk.vdi
-ln -s $GMS_PATH/opt-gms-disk.vdi $CWD/opt-gms-disk.vdi
+echo mv $CWD/../opt-gms-disk.vdi $GMS_PATH/
+mv $CWD/../opt-gms-disk.vdi $GMS_PATH/
+echo ln -s $GMS_PATH/opt-gms-disk.vdi $CWD/../opt-gms-disk.vdi
+ln -s $GMS_PATH/opt-gms-disk.vdi $CWD/../opt-gms-disk.vdi
 
 echo
 echo "Moving data virtual disk and creating a symlink to it"
-echo mv $CWD/data-disk.vdi $DATA_PATH/
-mv $CWD/data-disk.vdi $DATA_PATH/
-echo ln -s $DATA_PATH/data-disk.vdi $CWD/data-disk.vdi
-ln -s $DATA_PATH/data-disk.vdi $CWD/data-disk.vdi
+echo mv $CWD/../data-disk.vdi $DATA_PATH/
+mv $CWD/../data-disk.vdi $DATA_PATH/
+echo ln -s $DATA_PATH/data-disk.vdi $CWD/../data-disk.vdi
+ln -s $DATA_PATH/data-disk.vdi $CWD/../data-disk.vdi
 
 echo
 
