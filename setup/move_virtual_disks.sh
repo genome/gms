b320af19421259054df05aab3411ce754ce67323
Malachi Griffith <mgriffit@genome.wustl.edu>
2013-11-16 10:24:36 -0600

    Remove mounting of /opt/gms/$sys_id/ to sdd1 in the VM.  This may have been causing problems with the stability of data when rebooting the VM

--- setup/move_virtual_disks.sh
+++ setup/move_virtual_disks.sh
@@ -6,7 +6,6 @@
 #Update these to point to the volumes you wish to use on your host system
 TMP_PATH='/Volumes/GMS1'
 GMS_PATH='/Volumes/GMS2'
-#DATA_PATH='/Volumes/GMS2'
 
 #Check these paths
 if [ ! -d $TMP_PATH ]; then
@@ -17,10 +16,6 @@ if [ ! -d $GMS_PATH ]; then
   echo "*** Specified GMS_PATH does not exist: $GMS_PATH  ***"
   exit
 fi
-#if [ ! -d $DATA_PATH ]; then
-#  echo "*** Specified DATA_PATH does not exist: $DATA_PATH  ***"
-#  exit
-#fi
 
 CWD=$(dirname $0)
 
@@ -37,13 +32,6 @@ echo mv $CWD/../opt-gms-disk.vdi $GMS_PATH/
 mv $CWD/../opt-gms-disk.vdi $GMS_PATH/
 echo ln -s $GMS_PATH/opt-gms-disk.vdi $CWD/../opt-gms-disk.vdi
 ln -s $GMS_PATH/opt-gms-disk.vdi $CWD/../opt-gms-disk.vdi
-
-#echo
-#echo "Moving data virtual disk and creating a symlink to it"
-#echo mv $CWD/../data-disk.vdi $DATA_PATH/
-#mv $CWD/../data-disk.vdi $DATA_PATH/
-#echo ln -s $DATA_PATH/data-disk.vdi $CWD/../data-disk.vdi
-#ln -s $DATA_PATH/data-disk.vdi $CWD/../data-disk.vdi
 
 echo
 
