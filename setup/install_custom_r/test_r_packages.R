0c713003b50d04a77557b163da1bcef89e68b43a
Malachi Griffith <mgriffit@genome.wustl.edu>
2014-02-15 10:28:05 -0600

    add tests for success of R package installation to make process

--- setup/install_custom_r/test_r_packages.R
+++ setup/install_custom_r/test_r_packages.R
@@ -10,15 +10,26 @@ installed_package_list = c('bitops', 'Cairo', 'caTools', 'colorspace', 'doMC', '
                            'geneplotter', 'GenomeGraphs', 'lumi', 'methylumi', 'multtest', 'preprocessCore', 'ChemmineR',
                            'boot','copyCat', 'gplots', 'pbh5')
 
+package_count = length(installed_package_list)
+package_pass_count = 0
+package_fail_count = 0
+
 #Test packages installed properly (by install.packages or biocLite)
 for (package_name in installed_package_list){
   if (!package_name %in% rownames(installed.packages())){
     message=paste("ERROR: ", package_name, " package not installed successfully", sep="")
     print(message)
+    package_fail_count = package_fail_count+1
   }else{
     message=paste(package_name, " loaded", sep="")
     print(message);
+    package_pass_count = package_pass_count+1
   }
+}
+
+if ((package_pass_count == package_count) & (package_fail_count == 0)){
+  message=paste("R PACKAGE INSTALL SUCCESS. Installed ", package_count, " packages and their dependencies", sep="")
+  print(message)
 }
 
 #Display overall capabilities of this R installation
