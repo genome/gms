0ca016920eb08dd4bc213b4c83e0ffb4d378b34a
Ubuntu <ubuntu@ip-172-31-40-111>
2014-08-08 20:53:29 +0000

    add AWS preinstall script

---
+++ setup/aws/preinstall.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+#Preinstall priming script for AWS installs.
+
+sudo mkfs /dev/xvdb
+sudo mount /dev/xvdb /opt
+
+sudo mkfs /dev/xvdc
+sudo mount /dev/xvdc /opt
+
+cd /opt; mkdir src; cd src
+sudo chown -R ubuntu:ubuntu /opt
+
+sudo apt-get update
